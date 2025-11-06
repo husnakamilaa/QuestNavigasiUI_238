a1b595aca264c6691e63b703852de379e0532866
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 14:11:29 +0700

    feat: membuat button click menggunakan komponen button

--- app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
+++ app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
@@ -6,9 +6,11 @@
     import androidx.compose.foundation.layout.Column
     import androidx.compose.foundation.layout.Row
     import androidx.compose.foundation.layout.Spacer
+    import androidx.compose.foundation.layout.fillMaxWidth
     import androidx.compose.foundation.layout.height
     import androidx.compose.foundation.layout.padding
     import androidx.compose.foundation.layout.width
+    import androidx.compose.material3.Button
     import androidx.compose.material3.DividerDefaults.Thickness
     import androidx.compose.material3.ExperimentalMaterial3Api
     import androidx.compose.material3.HorizontalDivider
@@ -87,6 +89,12 @@
                     onValueChange = {},
                 )
                 Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(fraction = 1f),
+                    onClick = OnSubmitBtnClick
+                ) {
+                    Text(text = stringResource(id = R.string.submit))
+                }
             }
 
         }
