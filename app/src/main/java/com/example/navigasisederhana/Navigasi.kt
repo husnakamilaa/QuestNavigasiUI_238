f319e097f9e508ceab726f5d2b36842ef03d4ce9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 09:14:50 +0700

    feat: menambahkan enum class Navigasi dan mendefinisikan member konstan didalamnya

---
+++ app/src/main/java/com/example/navigasisederhana/Navigasi.kt
@@ -0,0 +1,14 @@
+package com.example.navigasisederhana
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
