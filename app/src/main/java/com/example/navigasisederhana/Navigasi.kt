6fbc7e360eccdac43979960ef8e93d95a9d8018e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 09:30:01 +0700

    feat: implementasi FormIsian pada navhost untuk mengarahkan start navigasi pada Formulir.name dan mengarahkannya pada Formulir.detail

--- app/src/main/java/com/example/navigasisederhana/Navigasi.kt
+++ app/src/main/java/com/example/navigasisederhana/Navigasi.kt
@@ -1,10 +1,14 @@
 package com.example.navigasisederhana
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -17,5 +21,20 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ) {
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    onSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
 }
