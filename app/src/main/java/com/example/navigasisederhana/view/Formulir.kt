55fa11e2fde4b039f19e89e066349a6b1dc7a29f
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-06 14:00:05 +0700

    feat: membuat dua radio button untuk semua yang ada di list jenisK menggunakan looping forEach dalam layout Row atau horizontal (satu baris)

--- app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
+++ app/src/main/java/com/example/navigasisederhana/view/Formulir.kt
@@ -4,12 +4,14 @@
 
     import androidx.compose.foundation.layout.Arrangement
     import androidx.compose.foundation.layout.Column
+    import androidx.compose.foundation.layout.Row
     import androidx.compose.foundation.layout.padding
     import androidx.compose.foundation.layout.width
     import androidx.compose.material3.DividerDefaults.Thickness
     import androidx.compose.material3.ExperimentalMaterial3Api
     import androidx.compose.material3.HorizontalDivider
     import androidx.compose.material3.OutlinedTextField
+    import androidx.compose.material3.RadioButton
     import androidx.compose.material3.Scaffold
     import androidx.compose.material3.Text
     import androidx.compose.material3.TopAppBar
@@ -54,7 +56,20 @@
                     .padding(20.dp)
                     .width(250.dp),
                     thickness = Thickness,
-                    color = Color.Red)
+                    color = Color.Red
+                )
+                Row {
+                    jenisK.forEach {
+                        item->
+                        Row(verticalAlignment = Alignment.CenterVertically){
+                            RadioButton(
+                                selected = false,
+                                onClick = {item}
+                            )
+                            Text(text = item)
+                        }
+                    }
+                }
             }
 
         }
